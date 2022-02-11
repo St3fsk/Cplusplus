3fd60345bcf4596d95b93518c92c1e103d3a31c0
Stef <99083931+St3fsk@users.noreply.github.com>
2022-02-11 15:54:55 +0100

    Update Polymorphism.cpp

--- Polymorphism.cpp
+++ Polymorphism.cpp
@@ -34,7 +34,11 @@ int main() {
   Animal myAnimal;
   Pig myPig;
   Dog myDog;
-
+  
+  // For example, think of a base class called Animal that has a method called animalSound(). 
+  // Derived classes of Animals could be pig, cat, dog or a bird.
+  // And they also have their own implementation of an animal sound.
+  
   myAnimal.animalSound();
   myPig.animalSound();
   myDog.animalSound();
